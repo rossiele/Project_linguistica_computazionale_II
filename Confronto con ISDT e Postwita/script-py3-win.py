e6739bce4e3a9406293b0c4f35a47decfd1a6814
Eleonora Rossi <61659420+rossiele@users.noreply.github.com>
2022-10-01 09:28:26 +0200

    Add files via upload

---
+++ Confronto con ISDT e Postwita/script-py3-win.py
@@ -0,0 +1,90 @@
+import argparse
+from argparse import RawTextHelpFormatter
+import re
+from nltk.metrics.agreement import AnnotationTask
+import os.path
+import sys
+
+def operation(file1, file2, op, unify):
+	lines1 = [x.split() for x in open(file1, "r", encoding='utf-8')]
+	lines2 = [x.split() for x in open(file2, "r", encoding='utf-8')]
+
+	if(op=="pos"):
+		min = 0
+		max = 10
+		target = 3
+	else:
+		min = 0
+		max = 10
+		target = 6
+	
+	output = "output-" + op + ".txt"
+	out_file = open(output,"w", encoding='utf-8')
+
+	count=0
+	for i in range(len(lines1)):
+		splitted_line1 = lines1[i]
+		splitted_line2 = lines2[i]
+
+		if len(splitted_line1) != len(splitted_line2):
+			print("Alignment error on line " + str(i+1))
+			exit()
+		if len(splitted_line1) != max and len(splitted_line1) != min:
+			print("Something is wrong at line " + str(i+1) + " in " + file1)
+			exit()
+		if len(splitted_line2) != max and len(splitted_line2) != min:
+			print("Something is wrong at line " + str(i+1) + " in " + file2)
+			exit()
+		if len(splitted_line1) == max:
+			if(splitted_line1[0] != splitted_line2[0]):
+				print("Alignment error on line " + str(i+1))
+				exit()
+			target1 = splitted_line1[target]
+			target2 = splitted_line2[target]					
+			if(op=='dep'):
+				target1 = target1 + "_" + splitted_line1[target + 1]
+				target2 = target2 + "_" + splitted_line2[target + 1]
+			
+			# prova di modifica
+			a='\t' + str(i+1) + '\t' + target1 + '\n'
+			b='\t' + str(i+1) + '\t' + target2 + '\n'
+			out_file.write("a "+a)
+			out_file.write("b "+b)
+			
+			if (a!=b):
+				count+=1
+				print("Differences found at line " + str(i+1) + ", token no. " + splitted_line1[0] + ", token " + splitted_line1[1])
+			
+			#out_file.write('a' + '\t' + str(i+1) + '\t' + target1 + '\n')
+			#out_file.write('b' + '\t' + str(i+1) + '\t' + target2 + '\n')
+				
+#			if op=="pos":
+#				if unify and splitted_line1[target] != splitted_line2[target]:
+#					print("Differences found at line " + str(i+1))
+#			if op=="dep":
+#				if unify and ((splitted_line1[target] != splitted_line2[target]) or (splitted_line1[target+1] != splitted_line2[target+1])):
+#					print("Differences found at line " + str(i+1))
+	print("Tot diff: ", count)
+	out_file.close()
+	annotation(output)
+
+def annotation(output):
+	t = AnnotationTask(data=[x.split() for x in open(output, "r", encoding='utf-8')])
+	print("\nAverage observed agreement: " + str(t.avg_Ao()))
+	print("\nKappa: " + str(t.kappa()));
+	
+
+def main(argv):
+	parser = argparse.ArgumentParser(description="Interannotator agreement", formatter_class=RawTextHelpFormatter)
+	parser.add_argument("command", choices=('pos', 'dep'), help="pos for interannotator agreement on pos\ndep interannotator agreement on dependencies")
+	parser.add_argument("-u", "--unify", action='store_true', help="helps the annotators to unify the files in a single one")
+	parser.add_argument("input1", help="one of the two files")
+	parser.add_argument("input2", help="the other file")
+	args = parser.parse_args()
+
+	operation(args.input1, args.input2, args.command, args.unify)
+
+	
+			
+if __name__ == "__main__":
+	main(sys.argv)
